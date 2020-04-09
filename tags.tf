718f1a4c04ea101f68e6be5a49bb2e74005acd4a
Doug Roccato <doug.roccato@updater.com>
2020-04-09 11:21:33 -0400

    add tagging for feature and component as optional

--- tags.tf
+++ tags.tf
@@ -1,17 +1,35 @@
-variable "owner_tag" {}
-variable "environment_tag" {}
-variable "product_tag" {}
-variable "division_tag" {}
+variable "owner_tag" {
+  description = "What team (or person if no team exists) is responsible for this resource"
+}
+variable "environment_tag" {
+  description = "What environment is this resource supporting (example: Prod, Staging/Sandbox, Demo)"
+}
+variable "product_tag" {
+  description = "What product is this resource a part of (exmple: UpdaterAPI, InvitePlatform, etc)"
+}
+variable "feature_tag" {
+  description = "What feature of a product does this component support? This is a 'subtag' of product"
+  default = ""
+}
+variable "component_tag" {
+  description = "What component of a product is this? This is a 'subtag' of product"
+  default = ""
+}
+variable "division_tag" {
+  description = "What division does this resource support (example: Updater, Verical1, UHS, etc)"
+}
 variable "project_tag" {
+  description = "What specific project is this a part of (usually shorter lived than products)"
   default = ""
 }
 variable "tf_workspace_tag" {
+  description = "The terraform cloud/enterprise workspace this resource was created from (example: Updater/core-ops-cons-infra-vault)"
   default = ""
 }
 
 locals {
-  tags = "${merge(map("Owner", var.owner_tag), map("Environment", var.environment_tag), map("Product", var.product_tag), map("Division", var.division_tag), map("Project", var.project_tag), map("TFWorkspace", var.tf_workspace_tag))}"
-  ruby_string_tags = "{ 'Owner' => '${var.owner_tag}', 'Environment' => '${var.environment_tag}', 'Product' => '${var.product_tag}', 'Division' => '${var.division_tag}', 'Project' => '${var.project_tag}', 'TFWorkspace' => '${var.tf_workspace_tag}' }"
+  tags = "${merge(map("Owner", var.owner_tag), map("Environment", var.environment_tag), map("Product", var.product_tag), map("Feature", var.feature_tag), map("Component", var.component_tag), map("Division", var.division_tag), map("Project", var.project_tag), map("TFWorkspace", var.tf_workspace_tag))}"
+  ruby_string_tags = "{ 'Owner' => '${var.owner_tag}', 'Environment' => '${var.environment_tag}', 'Product' => '${var.product_tag}', 'Feature' => '${var.feature_tag}', 'Component' => '${var.component_tag}', 'Division' => '${var.division_tag}', 'Project' => '${var.project_tag}', 'TFWorkspace' => '${var.tf_workspace_tag}' }"
 }
 
 
