b262bc3ab296a6df0b09d9be0de560f199e1ae9a
apollorion <joey@apollorion.com>
2021-12-06 14:12:19 -0500

    feat: support terraform 1.x

--- tags.tf
+++ tags.tf
@@ -10,33 +10,30 @@ variable "tf_workspace_tag" {
 }
 
 locals {
-  tags = "${merge(map("Owner", var.owner_tag), map("Environment", var.environment_tag), map("Product", var.product_tag), map("Division", var.division_tag), map("Project", var.project_tag), map("TFWorkspace", var.tf_workspace_tag))}"
+  tags = tomap({
+    "Owner"       = var.owner_tag,
+    "Environment" = var.environment_tag,
+    "Product"     = var.product_tag,
+    "Division"    = var.division_tag,
+    "Project"     = var.project_tag,
+    "TFWorkspace" = var.tf_workspace_tag
+  })
+
   ruby_string_tags = "{ 'Owner' => '${var.owner_tag}', 'Environment' => '${var.environment_tag}', 'Product' => '${var.product_tag}', 'Division' => '${var.division_tag}', 'Project' => '${var.project_tag}', 'TFWorkspace' => '${var.tf_workspace_tag}' }"
+
+  asg_tags = [for k, v in local.tags : { key = k, value = v, propagate_at_launch = true }]
 }
 
-
-# Testing here, will extract to a module once working
-
-data "null_data_source" "asg_tags" {
-  count = "${length(keys(local.tags))}"
-
-  inputs = "${merge(map(
-    "key", "${element(keys(local.tags), count.index)}",
-    "value", "${element(values(local.tags), count.index)}"
-  ),
-  map("propagate_at_launch", "true"))
-  }"
-}
 
 output "asg_tags" {
-  value       = ["${data.null_data_source.asg_tags.*.outputs}"]
+  value       = local.asg_tags
   description = "Tags as a list of maps for ASGs"
 }
 
 output "tags" {
-  value = "${local.tags}"
+  value = local.tags
 }
 
 output "ruby_string_tags" {
-  value = "${local.ruby_string_tags}"
+  value = local.ruby_string_tags
 }
